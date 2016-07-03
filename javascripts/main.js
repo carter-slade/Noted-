ca47a69698bee579f311262c2d362e2f3cdc4a13
carter-slade <deepak19395@yahoo.com>
2016-07-03 20:53:15 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
-console.log('This would be the main JS file.');
+window.onbeforeunload = myfunction;
+	window.onload = function(e){
+
+		var stored = localStorage.getItem("notedtext");
+		// console.log(stored);
+		if(stored!="")
+		{
+			if (typeof(Storage) !== "undefined")
+			{
+				document.getElementById('notetext').innerHTML = stored;
+			}
+		}
+
+	}
+	function myfunction() {
+		
+		// console.log("key pressed");
+		var text = document.getElementById('notetext').value;
+		// console.log(text);
+		if (typeof(Storage) !== "undefined")
+		{
+			localStorage.setItem("notedtext",text);
+		}
+		// body...
+	}
